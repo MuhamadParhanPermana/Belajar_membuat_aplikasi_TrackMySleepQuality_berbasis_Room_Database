79cc0e3ebd6d17690cf85d1e756676f405fe7814
MuhamadParhanPermana <muhamadparhanpermana71@gmail.com>
2022-04-18 15:04:20 -0700

    update navigation and changed viewBinding with dataBinding

--- app/src/main/java/com/example/trackmysleepquality/Util.kt
+++ app/src/main/java/com/example/trackmysleepquality/Util.kt
@@ -1,5 +1,3 @@
-package com.example.trackmysleepquality
-
 /*
  * Copyright 2019, The Android Open Source Project
  *
@@ -16,9 +14,42 @@ package com.example.trackmysleepquality
  * limitations under the License.
  */
 
+package com.example.trackmysleepquality
+
 import android.annotation.SuppressLint
 import android.content.res.Resources
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
+import com.example.trackmysleepquality.database.SleepNight
 import java.text.SimpleDateFormat
+//import java.util.concurrent.TimeUnit
+//import java.util.*
+
+
+//private val ONE_MINUTE_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES)
+//private val ONE_HOUR_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS)
+
+//fun convertDurationToFormatted(startTimeMilli: Long, endTimeMilli: Long, res: Resources): String {
+//    val durationMilli = endTimeMilli - startTimeMilli
+//    val weekdayString = SimpleDateFormat("EEEE", Locale.getDefault()).format(startTimeMilli)
+//    return when {
+//        durationMilli < ONE_MINUTE_MILLIS -> {
+//            val seconds = TimeUnit.SECONDS.convert(durationMilli, TimeUnit.MILLISECONDS)
+//            res.getString(R.string.seconds_length, seconds, weekdayString)
+//        }
+//        durationMilli < ONE_HOUR_MILLIS -> {
+//            val minutes = TimeUnit.MINUTES.convert(durationMilli, TimeUnit.MILLISECONDS)
+//            res.getString(R.string.minutes_length, minutes, weekdayString)
+//        }
+//        else -> {
+//            val hours = TimeUnit.HOURS.convert(durationMilli, TimeUnit.MILLISECONDS)
+//            res.getString(R.string.hours_length, hours, weekdayString)
+//        }
+//    }
+//}
+
+
 
 /**
  * These functions create a formatted string that can be set in a TextView.
@@ -69,32 +100,36 @@ fun convertLongToDateString(systemTime: Long): String {
  * @return  Spanned - An interface for text that has formatting attached to it.
  *           See: https://developer.android.com/reference/android/text/Spanned
  */
-//fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
-//    val sb = StringBuilder()
-//    sb.apply {
-//        append(resources.getString(R.string.title))
-//        nights.forEach {
-//            append("<br>")
-//            append(resources.getString(R.string.start_time))
-//            append("\t${convertLongToDateString(it.startTimeMilli)}<br>")
-//            if (it.endTimeMilli != it.startTimeMilli) {
-//                append(resources.getString(R.string.end_time))
-//                append("\t${convertLongToDateString(it.endTimeMilli)}<br>")
-//                append(resources.getString(R.string.quality))
-//                append("\t${convertNumericQualityToString(it.sleepQuality, resources)}<br>")
-//                append(resources.getString(R.string.hours_slept))
-//                // Hours
-//                append("\t ${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60 / 60}:")
-//                // Minutes
-//                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60}:")
-//                // Seconds
-//                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000}<br><br>")
-//            }
-//        }
-//    }
-//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//        return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
-//    } else {
-//        return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
-//    }
-//}
+fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
+    val sb = StringBuilder()
+    sb.apply {
+        append(resources.getString(R.string.title))
+        nights.forEach {
+            append("<br>")
+            append(resources.getString(R.string.start_time))
+            append("\t${convertLongToDateString(it.startTimeMilli)}<br>")
+            if (it.endTimeMilli != it.startTimeMilli) {
+                append(resources.getString(R.string.end_time))
+                append("\t${convertLongToDateString(it.endTimeMilli)}<br>")
+                append(resources.getString(R.string.quality))
+                append("\t${convertNumericQualityToString(it.sleepQuality, resources)}<br>")
+                append(resources.getString(R.string.hours_slept))
+                // Hours
+                append("\t ${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60 / 60}:")
+                // Minutes
+                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60}:")
+                // Seconds
+                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000}<br><br>")
+            }
+        }
+    }
+    // fromHtml is deprecated for target API without a flag, but since our minSDK is 19, we
+    // can't use the newer version, which requires minSDK of 24
+    //https://developer.android.com/reference/android/text/Html#fromHtml(java.lang.String,%20int)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
+    } else {
+        @Suppress("DEPRECATION")
+        return Html.fromHtml(sb.toString())
+    }
+}
